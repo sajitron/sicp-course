672e5075183710b0b9d561bb499391c6a1b06d05
Adams Banjo <sajioloye@gmail.com>
2022-10-31 03:41:47 +0000

    exercise 1.3

--- main.scm
+++ main.scm
@@ -4,4 +4,10 @@
 
 (define (sum-of-squares x y) (+ (square x) (square y)))
 
-(sum-of-squares 3 4)
+(define (sum-square-largest-two a b c) (cond 
+                                            ((and (<= a b) (<= a c)) (sum-of-squares b c))
+                                            ((and (<= b a) (<= b c)) (sum-of-squares a c))
+                                            (else (sum-of-squares a b))
+))
+
+(sum-square-largest-two 3 6 2)
