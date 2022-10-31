78b7589900aeacd0c952566fafcb1805750abf6d
Adams Banjo <sajioloye@gmail.com>
2022-10-31 03:48:04 +0000

    conditionals

--- main.scm
+++ main.scm
@@ -4,4 +4,10 @@
 
 (define (sum-of-squares x y) (+ (square x) (square y)))
 
-(sum-of-squares 3 4)
+(define (sum-square-largest x y z) (cond
+                                    ((and (<= x y) (<= x z)) (sum-of-squares y z))
+                                    ((and (<= y z) (<= y x)) (sum-of-squares x z))
+                                    (else (sum-of-squares x y))))
+
+
+(sum-square-largest 3 6 2)
